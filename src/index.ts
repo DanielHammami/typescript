081692b83b3d3cc73332599d9d1bb27ffc53d22a
Daniel <daniel.hammami@yahoo.com>
2022-10-08 15:22:22 +0200

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-// 1, "Mosh"
-let user: [number, string] = [1, "Mosh"];
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize)
