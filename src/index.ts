35ee05091a49090306f7324f488b70edee251122
Daniel <daniel.hammami@yahoo.com>
2022-10-11 16:40:21 +0200

    access_control_keywords

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
-  constructor(id: number, owner: string, balance: number) {
+  constructor(id: number, owner: string, _balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = _balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
-    this.balance += amount;
+    this._balance += amount;
+  }
+
+  private calculateTax(): void {}
+
+  getBalance(): number {
+    return this._balance;
   }
 }
 
 let account = new Account(1, "Mosh", 0);
 account.deposit(100);
-console.log(account instanceof Account);
+console.log(account);
