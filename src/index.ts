571449208e997b7e0ac0477ece4f6aa851ee80d4
Daniel <daniel.hammami@yahoo.com>
2022-10-11 15:15:16 +0200

    exercices

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-function reject(message: string): never {
-  throw new Error(message);
-}
+type Users = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
 
-function processEvents(): never {
-  while (true) {
-    // Do something
-  }
-}
+let users: Users[] = [
+  {
+    name: "John Smith",
+    age: 30,
+    occupation: "Software engineer",
+  },
+  {
+    name: "Kate Müller",
+    age: 28,
+  },
+];
 
-//processEvents();
-reject("Error");
-console.log("Hello World");
+/////////////////////////////////////
+
+type Bird = {
+  fly: () => void;
+};
+
+type Fish = {
+  swim: () => void;
+};
+
+type Pet = Bird | Fish;
+
+/////////////////////////////////////
+
+const getUser = () => void;
+const bar = () => void;
+let foo;
+
+let user = getUser();
+console.log((user?.address?.street);
+let x = foo ?? bar();
+
+/////////////////////////////////////
+
+let value: unknown = "a";
+if (typeof value === "string")
+  console.log(value.toUpperCase())
