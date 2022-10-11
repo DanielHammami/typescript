228374a0182d583255c9e884c0597881a87e57e3
Daniel <daniel.hammami@yahoo.com>
2022-10-11 15:43:19 +0200

    creating_classes

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,16 @@
-type Users = {
-  name: string;
-  age: number;
-  occupation?: string;
-};
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
 
-let users: Users[] = [
-  {
-    name: "John Smith",
-    age: 30,
-    occupation: "Software engineer",
-  },
-  {
-    name: "Kate Müller",
-    age: 28,
-  },
-];
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
 
-/////////////////////////////////////
-
-type Bird = {
-  fly: () => void;
-};
-
-type Fish = {
-  swim: () => void;
-};
-
-type Pet = Bird | Fish;
-
-/////////////////////////////////////
-
-const getUser = () => void;
-const bar = () => void;
-let foo;
-
-let user = getUser();
-console.log((user?.address?.street);
-let x = foo ?? bar();
-
-/////////////////////////////////////
-
-let value: unknown = "a";
-if (typeof value === "string")
-  console.log(value.toUpperCase())
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this.balance += amount;
+  }
+}
